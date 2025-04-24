9c334a8de038d95e40c7650e0532e91f94461613
NarasetS <80676034+NarasetS@users.noreply.github.com>
2025-04-24 13:29:47 +0700

    6

--- update_data/updatedata.py
+++ update_data/updatedata.py
@@ -38,10 +38,10 @@ df_list_stock = df_list_stock.reset_index(drop=True)
 
 print(df_list_stock)
 
-# self_lib.etl.etl(df_list_stock,'s&p500')
-# self_lib.kmeanclustering.kmeanclustering('s&p500')
-# self_lib.etl.etl(df_list_stock,'SET')
-# self_lib.kmeanclustering.kmeanclustering('SET')
+self_lib.etl.etl(df_list_stock,'s&p500')
+self_lib.kmeanclustering.kmeanclustering('s&p500')
+self_lib.etl.etl(df_list_stock,'SET')
+self_lib.kmeanclustering.kmeanclustering('SET')
 self_lib.etl.etl(df_list_stock,'us')
 self_lib.kmeanclustering.kmeanclustering('us')
 
